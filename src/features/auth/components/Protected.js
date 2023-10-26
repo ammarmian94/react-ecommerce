f6d024c3b995150399d835773dbced6612d8f787
ammarmian94 <96662437+ammarmian94@users.noreply.github.com>
2023-10-27 00:23:35 +0500

    frontend part 11-1 login signup completed

---
+++ src/features/auth/components/Protected.js
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { selectLoggedInUser } from "../authSlice";
+
+export default function Protected({ children }) {
+  const user = useSelector(selectLoggedInUser);
+  if (!user) return <Navigate to="/login"></Navigate>;
+  return children;
+}
--- src/features/auth/components/SignUp.js
+++ src/features/auth/components/SignUp.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { selectLoggedInUser, createUserAsync } from "../authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignUp() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
+
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  // console.log(errors);
   return (
     <div>
+      {/* {user?.email} */}
+      {user && <Navigate to="/" replace={true}></Navigate>}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -24,7 +32,15 @@ export default function SignUp() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form
+            noValidate
+            className="space-y-6"
+            onSubmit={handleSubmit((data) => {
+              dispatch(
+                createUserAsync({ email: data.email, password: data.password })
+              );
+            })}
+          >
             <div>
               <label
                 htmlFor="email"
@@ -45,7 +61,9 @@ export default function SignUp() {
                   type="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                <p className="text-red-500">{errors.email.message}</p>
+                {errors.email && (
+                  <p className="text-red-500">{errors.email.message}</p>
+                )}
               </div>
             </div>
 
@@ -62,12 +80,22 @@ export default function SignUp() {
               <div className="mt-2">
                 <input
                   id="password"
-                  name="password"
+                  {...register("password", {
+                    required: "password is required",
+                    pattern: {
+                      value:
+                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+                      message: `- at least 8 characters\n
+                              - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number\n
+                              - Can contain special characters`,
+                    },
+                  })}
                   type="password"
-                  autoComplete="current-password"
-                  required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {errors.password && (
+                  <p className="text-red-500">{errors.password.message}</p>
+                )}
               </div>
             </div>
 
@@ -83,12 +111,20 @@ export default function SignUp() {
 
               <div className="mt-2">
                 <input
-                  id="confirm-password"
-                  name="confirm-password"
+                  id="confirmPassword"
+                  {...register("confirmPassword", {
+                    required: "password do not match",
+                    validate: (value, formValues) =>
+                      value === formValues.password || "password do not match",
+                  })}
                   type="password"
-                  required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {errors.confirmPassword && (
+                  <p className="text-red-500">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
               </div>
             </div>
 
