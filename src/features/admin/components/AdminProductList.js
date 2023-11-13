fd6429a33b51e57e7fb83b7e5c2c28597ffb9a30
unknown <justdoitdude9@gmail.com>
2023-11-13 14:10:12 +0500

    SL finishing up frontend

--- src/features/admin/components/AdminProductList.js
+++ src/features/admin/components/AdminProductList.js
@@ -454,6 +454,9 @@ function ProductGrid({ products }) {
                     <p className="text-sm text-red-400">Product Deleted</p>
                   </div>
                 )}
+                {product.stock <=0 && (
+                    <div><p className="text-sm text-red-400">out of stock</p></div>
+                  )}
                 <div className="mt-5">
                   <Link
                     to={`/admin/product-form/edit/${product.id}`}
